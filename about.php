4714cf0180799ff5a46c681d04534c902787d835
rlarkin01 <44369115+rlarkin01@users.noreply.github.com>
2021-11-15 20:21:44 -0500

    small change to trigger pipeline

--- about.php
+++ about.php
@@ -37,6 +37,7 @@ $page[ 'body' ] .= "
 		<li>Tedi Heriyanto: " . dvwaExternalLinkUrlGet( 'http://tedi.heriyanto.net/','tedi.heriyanto.net' ) . "</li>
 		<li>Tom Mackenzie</li>
 		<li>Robin Wood: " . dvwaExternalLinkUrlGet( 'https://digi.ninja/','digi.ninja' ) . "</li>
+		<li>Robin Hood: " . dvwaExternalLinkUrlGet( 'https://digi.ninja/','digi.ninja' ) . "</li>
 	</ul>
 	<ul>
 		<li>PHPIDS - Copyright (c) 2007 " . dvwaExternalLinkUrlGet( 'http://github.com/PHPIDS/PHPIDS', 'PHPIDS group' ) . "</li>
